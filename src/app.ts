9acd059770900e345a5a4e7621c5b843d521da2c
DeyvisonTav <deyvisontav@gmail.com>
2023-03-21 05:37:06 -0300

    fix: cors/app register

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
-import fastify from "fastify";
+import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { appRoutes } from "./http/routes";
 
-export const app = fastify();
+export const app = Fastify();
+app.register(cors, {
+  origin: (_origin, cb) => {
+    cb(null, true);
+  },
+});
 
 app.register(appRoutes);
